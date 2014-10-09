5784e9feaacc6ff4ac0cade6bb8b54e252ee6881
hillionh <hillionh@berkeley.edu>
2014-10-08 19:26:27 -0700

    description

---
+++ Caltranscense/GMM.py
@@ -0,0 +1,148 @@
+# coding utf-8
+
+import numpy as np
+from scipy.stats import norm
+
+
+class Gaussian:
+
+    def __init__(self, mean=0, variance=1, weight=1):
+        self.mean = mean
+        self.variance = variance
+        self.weight = weight
+
+    def eval_proba(self, x=None):
+        #         print("weight", self.weight, "x", x, "mean",
+        #               self.mean, "var", np.matrix(self.variance))
+        #         print("re", self.weight * norm.pdf(x, self.mean, self.variance))
+        return(self.weight * norm.pdf(x, self.mean, self.variance))
+
+    def eval_post_proba(self, x=None, model=None):
+        return(self.eval_proba(x) / model.eval_proba(x))
+
+    def update_parameters(self, mean=None, variance=None, weight=None):
+        if mean != None:
+            self.mean = mean
+        if variance != None:
+            self.variance = variance
+        if weight != None:
+            self.weight = weight
+
+
+class MixtureModel:
+
+    def __init__(self):
+        self.gaussians = []
+        self.parameters = {'means': [], 'variances': [], 'weights': []}
+
+    def add_Gaussian(self, gaussian):
+        self.gaussians += [gaussian]
+
+    def eval_proba(self, x):
+        res = 0
+        for gaussian in self.gaussians:
+            res += gaussian.eval_proba(x)
+        return(res)
+
+    def set_parameters(self):
+        for gaussian in self.gaussians:
+            self.parameters['means'] += [gaussian.mean]
+            self.parameters['variances'] += [gaussian.variance]
+            self.parameters['weights'] += [gaussian.weight]
+
+    def update_parameters(self, means=None, variances=None, weights=None):
+        self.parameters['means'] = means
+        self.parameters['variances'] = variances
+        self.parameters['weights'] = weights
+
+    def get_parameters(self):
+        return(self.parameters)
+
+
+
+def EM_algorithm(max_iter=100, S=None):
+    '''N is the number of speakers, K is the number of features per signal, S is the set of samples'''
+
+    # check the validity of the samples set
+    if S == None or S == []:
+        print("Error : set of samples empty")
+        return(None)
+
+    # Initializations
+    N = len(S)  # number of speakers
+    K = len(S[0])  # number of samples
+    D = len(S[0][0])  # number of features, ie vectors dimension
+
+#     print("N=" + str(N), "K=" + str(K))
+
+    model = MixtureModel()
+    for n in range(N):
+        model.add_Gaussian(
+            Gaussian(mean=0, variance=np.identity(D), weight=1. / N))
+    model.set_parameters()
+#     print(model.parameters)
+
+    # EM loop
+    counter = 0
+    converged = False
+    while not converged:
+        #print('test 1 ok')
+        proba = []
+        means = []
+        variances = []
+        weights = []
+        speaker_id = 0
+
+        for speaker in S:
+
+            proba += [[]]
+            k = 0
+
+            for sample in speaker:
+
+                # compute  probability of having sample s in mixture k
+                proba[-1] += [float(
+                    model.gaussians[speaker_id].eval_proba(sample) / model.eval_proba(sample))]
+                k += 1
+
+            speaker_id += 1
+
+        for k in range(N):
+
+            # compute weight of mixture k (N-list of 1-Dim)
+            weights += [sum([proba[k][i]
+                             for i in range(K)]) / K]
+#             print(weights[-1])
+
+            # compute mean of mixture k (N-list of N-Dim vector)
+#             print('1',proba[k][i])
+#             print('2', S[k][i][0])
+#             print('3',sum([np.dot(proba[k][i], S[k][i][0]) for i in range(K)]))
+#             print('4', 1. / sum([proba[k][i] for i in range(K)]))
+            means += [sum([np.dot(proba[k][i], S[k][i][d]) for i in range(
+                K)]) * 1. / sum([proba[k][i] for i in range(K)]) for d in range(D)]
+#             print(means[-1])
+
+            # compute variance of mixture k (N-list of (N*N)-Dim matrix)
+#             print(proba[k][i])
+#             print(S[k][i])
+#             print(means[-1])
+#             print('sum', sum([proba[k][i] * np.dot((S[k][i] - means[-1]), (S[k][i] - means[-1]).transpose()) for i in range(K)]))
+#             print('sum2', sum([proba[k][i] for i in range(K)]))
+            variances += [sum([proba[k][i] * np.dot((S[k][i] - means[-1]), (S[k][i] - means[-1]).transpose()) for i in range(K)]
+                                         ) / sum([proba[k][i] for i in range(K)])]
+#             print(variances[-1])
+            # unfinished business
+
+#         print(weights)
+#         print(means)
+#         print(variances)
+
+        # update parameters
+        model.update_parameters(means, variances, weights)
+
+        # Convergence check
+        counter += 1
+        converged = counter >= max_iter
+
+    return(model)
---
+++ Caltranscense/main.py
@@ -0,0 +1,59 @@
+# coding utf-8
+
+
+from GMM import EM_algorithm
+
+# input : audio files directories
+global audio_file_list_dir
+
+audio_file_list_dir = {'Antonio': None, 'Helene': None,
+                       'Leo': None, 'Mathilde': None, 'Nigil': None}
+
+# define features extraction processing
+
+
+def feat_extract(audio_file=None):
+    # unfinished business
+    return(audio_file)
+
+
+# define class to store info
+class personal_data:
+
+    def __init__(self, name):
+        self.name = name
+        self.audio_files = None
+        self.audio_file_list_dir = audio_file_list_dir[name]
+        self.features_list = None
+
+    def add_data(self):
+        for audio_file in self.audio_file_list_dir:
+            self.audio_files += [audio_file]
+
+    def extract_features(self):
+        for audio_file in self.audio_files:
+            self.features_list += [feat_extract(audio_file)]
+
+# create classes
+antonio_data = personal_data('Antonio')
+helene_data = personal_data('Helene')
+leo_data = personal_data('Leo')
+mathilde_data = personal_data('Mathilde')
+nigil_data = personal_data('Nigil')
+
+data_list = [antonio_data, helene_data, leo_data, mathilde_data, nigil_data]
+
+# extract features and build samples list
+S = []
+for data_element in data_list:
+    data_element.add_data()
+    data_element.extract_features()
+    S += [data_element.features_list]
+
+
+# parameter estimation : EM algorithm
+model = EM_algorithm(S)
+
+# print parameters and visualize gaussians
+print(model.get_parameters)
+# unfinished business
